9a99a41c4dd86b393d3acdea3c1133c79c0b8cbf
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2023-11-29 11:03:57 +0900

    add appsscript.json.

--- dist/code.js
+++ dist/code.js
@@ -1,68 +1,37 @@
-function betterConvertMIMETypeOfEMFs() {
-  const oneWeekAgo = new Date();
-  oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-
-  const targetMimeType = 'image/emf';
+function getFileExtension_(file) {
+  // Split the filename with a half-width period and get the last element (extension).
+  return file
+    .getName()
+    .split('.')
+    .filter((_, idx, arr) => idx === arr.length - 1)[0];
+}
+function convertMIMETypeOfEMFs() {
+  const emfExtension = 'emf';
   const desiredMimeType = 'application/x-msmetafile';
-  const query =
-    'mimeType != "' +
-    desiredMimeType +
-    '" and fileExtension = "emf" and modifiedDate > "' +
-    oneWeekAgo.toISOString() +
-    '"';
-  const files = DriveApp.searchFiles(query);
+  const drawingMimeType = 'application/vnd.google-apps.drawing';
+  // Get the folder where the script is stored.
+  const folder = DriveApp.getFileById(ScriptApp.getScriptId())
+    .getParents()
+    .next();
+  const files = folder.getFiles();
   while (files.hasNext()) {
     const file = files.next();
-    const parents = file.getParents();
-    if (parents.hasNext()) {
-      const folder = parents.next();
-      if (
-        file.getOwner().getEmail() === 'mariko.ohtsuka@nnh.go.jp' &&
-        file.getName() === 'コピーFigure_OS_total.emf'
-      ) {
-        const test = file.getName();
-        console.log(test);
-        // Try to get the blob and change MIME type; if error, skip this file
-        try {
-          const blob = file.getBlob();
-          blob.setContentType(desiredMimeType);
-          folder.createFile(blob);
-        } catch (e) {
-          console.log(
-            'Error processing file ' + file.getName() + ': ' + e.toString()
-          );
-          continue; // Skip this file and move to the next one
-        }
-      }
-    }
-  }
-  /* 
-
-  while (files.hasNext() && count < maxFilesToConvert) {
-    var file = files.next();
-
-    // Check if file has parents before attempting to retrieve
-    var parents = file.getParents();
-    if (parents.hasNext()) {
-      var folder = parents.next();
-     
-      // Try to get the blob and change MIME type; if error, skip this file
+    if (
+      getFileExtension_(file) === emfExtension &&
+      file.getMimeType() !== desiredMimeType &&
+      file.getMimeType() !== drawingMimeType
+    ) {
+      // Try to get the blob and change MIME type; if error, skip this file.
       try {
-        var blob = file.getBlob();
+        console.log(`convert MimeType :${file.getName()}`);
+        console.log(file.getMimeType());
+        const blob = file.getBlob();
         blob.setContentType(desiredMimeType);
         folder.createFile(blob);
-        count++;
       } catch (e) {
-        Logger.log("Error processing file " + file.getName() + ": " + e.toString());
-        continue;  // Skip this file and move to the next one
+        console.log(`Error processing file${file.getName()}: ${e.toString()}`);
+        continue; // Skip this file and move to the next one.
       }
     }
   }
-
-  if (count === 0) {
-    Logger.log('No recent EMF files needing conversion were found.');
-  } else {
-    Logger.log(count + ' recent EMF files converted to desired MIME type.');
-  }
-*/
 }
